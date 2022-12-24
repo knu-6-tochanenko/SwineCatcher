6832093983fbbbca0af5fc64d4785ca58e9b7d42
Vladyslav Tochanenko <itochanenko@gmail.com>
2022-12-24 22:53:21 +0200

    Implement tank control

--- js/move_engine.js
+++ js/move_engine.js
@@ -1,21 +1,23 @@
+// DOM Elements
 var divScore = document.getElementById('score');
+var divLives = document.getElementById('lives');
 
-function getRandomInt(max) {
-	return Math.floor(Math.random() * max);
-}
-
+// Game settings
 const SPEED_LIMIT = 3;
 const BOWL_SIZE = 80;
 const SWINE_SIZE = 80;
 const GAME_SPEED = 60;
+const TANK_SPEED = 3;
+const TANK_GAME_SPEED = 10;
+const SWINE_SPAWN = 2000;
 
+// Game stats
 var CONFIG = {
 	SCORE: 0,
 	LIVES: 10
 }
 
-divScore.innerHTML = CONFIG.LIVES;
-
+// Variables
 const globalHeight = document.body.scrollHeight;
 const globalWidth = document.body.scrollWidth;
 
@@ -25,19 +27,18 @@ const bowlCoords = {
 	top: (globalHeight - BOWL_SIZE) / 2,
 	bottom: (globalHeight + BOWL_SIZE) / 2
 }
-
-var movableObject = {
-	pos_x: 0,
-	pos_y: 0,
-	speed_x: 0,
-	speed_y: 0,
-	id: "",
-	interval: {}
-}
-
 var swineCount = 0;
+var swines = [];
+var tankConfig = {
+	pos_x: (globalWidth - BOWL_SIZE) / 2,
+	pos_y: (globalHeight - BOWL_SIZE) / 2,
+	speed_x: 0,
+	speed_y: 0
+};
 
-var movableObjects = [];
+function getRandomInt(max) {
+	return Math.floor(Math.random() * max);
+}
 
 function collide(swine, obj) {
 	let swineCoords = {
@@ -53,6 +54,54 @@ function collide(swine, obj) {
 		swineCoords.bottom <= obj.top ||
 		swineCoords.left >= obj.right
 	);
+}
+
+function createTank() {
+	var tank = document.getElementById('tank');
+	let interval = setInterval(tankFrame, TANK_GAME_SPEED);
+
+	function tankFrame() {
+		let tankCoords = {
+			left: tankConfig.pos_x - BOWL_SIZE / 2,
+			right: tankConfig.pos_x + BOWL_SIZE / 2,
+			top: tankConfig.pos_y - BOWL_SIZE / 2,
+			bottom: tankConfig.pos_y + BOWL_SIZE / 2
+		}
+
+		for (var i = 0; i < swines.length; i++) {
+			if (collide(swines[i], tankCoords)) {
+				console.log(JSON.stringify(swines[i], null, 4));
+				let swineElement = document.getElementById(swines[i].id);
+				clearInterval(swines[i].interval);
+				
+				swineElement.remove();
+				
+				swines.splice(i, 1);
+				CONFIG.SCORE++;
+				divScore.innerHTML = CONFIG.SCORE + ' points';
+				break;
+			}
+		}
+
+		if (tankConfig.pos_x <= 0 && tankConfig.speed_x == -TANK_SPEED) {
+			tankConfig.speed_x = 0;
+		}
+		if (tankConfig.pos_x >= globalWidth && tankConfig.speed_x == TANK_SPEED) {
+			tankConfig.speed_x = 0;
+		}
+		if (tankConfig.pos_y <= 0 && tankConfig.speed_y == -TANK_SPEED) {
+			tankConfig.speed_y = 0;
+		}
+		if (tankConfig.pos_y >= globalHeight && tankConfig.speed_y == TANK_SPEED) {
+			tankConfig.speed_y = 0;
+		}
+
+		tankConfig.pos_x = tankConfig.pos_x + tankConfig.speed_x;
+		tank.style.left = tankConfig.pos_x - (SWINE_SIZE / 2) + 'px';
+
+		tankConfig.pos_y = tankConfig.pos_y + tankConfig.speed_y;
+		tank.style.top = tankConfig.pos_y - (SWINE_SIZE / 2) + 'px';
+	}
 }
 
 function createSwine() {
@@ -98,9 +147,15 @@ function createSwine() {
 
 	swine.style.top = newSwine.pos_y - (SWINE_SIZE / 2) + 'px';
 	swine.style.left = newSwine.pos_x - (SWINE_SIZE / 2) + 'px';
-	swine.id = "swine" + (swineCount++);
+	newSwine.id = "swine" + (swineCount++);
+	swine.setAttribute('id', newSwine.id);
+
+	
 	document.body.appendChild(swine);
 	let interval = setInterval(frame, GAME_SPEED);
+	newSwine.interval = interval;
+	
+	swines.push(newSwine);
 
 	function frame() {
 		if (!collide(newSwine, bowlCoords)) {
@@ -110,12 +165,54 @@ function createSwine() {
 			newSwine.pos_y = newSwine.pos_y + newSwine.speed_y;
 			swine.style.top = newSwine.pos_y - (SWINE_SIZE / 2) + 'px';
 		} else {
-			clearInterval(interval);
+			clearInterval(newSwine.interval);
 			swine.remove();
 			CONFIG.LIVES--;
-			divScore.innerHTML = CONFIG.LIVES;
+			divLives.innerHTML = CONFIG.LIVES;
+
+			swines = swines.filter(function( obj ) {
+				return obj.id !== newSwine.id;
+			});
+
 		}
 	}
 }
 
-setInterval(createSwine, 2000);
+document.addEventListener('keydown', moveTank);
+
+function moveTank(event) {
+	if (event.isComposing || event.keyCode === 87 || event.keyCode === 38) {
+		console.log("UP");
+		tankConfig.speed_x = 0;
+		tankConfig.speed_y = -TANK_SPEED;
+		return;
+	}
+	if (event.isComposing || event.keyCode === 65 || event.keyCode === 37) {
+		console.log("LEFT");
+		tankConfig.speed_x = -TANK_SPEED;
+		tankConfig.speed_y = 0;
+		return;
+	}
+	if (event.isComposing || event.keyCode === 83 || event.keyCode === 40) {
+		console.log("BOTTOM");
+		tankConfig.speed_x = 0;
+		tankConfig.speed_y = TANK_SPEED;
+		return;
+	}
+	if (event.isComposing || event.keyCode === 68 || event.keyCode === 39) {
+		console.log("RIGHT");
+		tankConfig.speed_x = TANK_SPEED;
+		tankConfig.speed_y = 0;
+		return;
+	}
+	if (event.isComposing || event.keyCode === 32) {
+		console.log("RIGHT");
+		tankConfig.speed_x = 0;
+		tankConfig.speed_y = 0;
+	}
+}
+
+divScore.innerHTML = CONFIG.SCORE;
+divLives.innerHTML = CONFIG.LIVES;
+createTank();
+setInterval(createSwine, SWINE_SPAWN);
